0b14811109cc401af9a9febfb1652b5ba1974566
Yash Janyani <yash.janyani2000@gmail.com>
2022-07-15 15:52:41 +0530

    translation step 1

--- src/App.js
+++ src/App.js
@@ -4,24 +4,40 @@ import TableView from "./components/TableView";
 import { Layout, Col, Row } from 'antd';
 import React from 'react';
 import "./App.css";
+import TranslationTest from "../src/components/TranslationTest";
+// import LanguageButton from "./components/LanguageButton";
+import i18n from '../src/translations/i18n';
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Content } = Layout;
 
 function App() {
+  const { t } = useTranslation();
+  const [language, setLanguage] = useState('en');
+ 
+  function handleOnclick(e){
+    e.preventDefault();
+    setLanguage(e.target.value);
+    i18n.changeLanguage(e.target.value);
+  }
+
   return (
     <Layout>
       <Header className="header">
       <h1>EMBARKATION MONITORING</h1>
       </Header>
-     <Content className="content">1. TABLE VIEW</Content>
+      <div><button value='en' onClick={handleOnclick}>
+        English
+     </button>
+
+<button value='zh' onClick={handleOnclick}>
+ Chinese
+</button></div>
+      <TranslationTest />
+     <Content className="content">1. TABLE VIEW
+     </Content>
      <TableView />
-     {/* <div className="graph">
-      
-     <Content className="content"></Content>
-     
-     <Content className="content"></Content>
-     
-     </div> */}
      <Row className="graph">
       <Col span={12} className="content">2. LINE GRAPH <DemoLine /></Col>
       <Col span={12} className="content">3. BAR GRAPH <DemoColumn /></Col>
---
+++ src/translations/zh/translation.js
@@ -0,0 +1,4 @@
+export const TRANSLATIONS_ZH = {
+  welcome:"欢迎使用本教程",
+  title:'monitoreo de embarque'
+ };
